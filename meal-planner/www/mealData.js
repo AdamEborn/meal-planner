184ccb5e14b756fa7b6076b85ed9de16d44c4c1e
adam-eborn <adam.eborn@nhs.net>
2017-09-09 17:57:08 +0100

    first commit

---
+++ meal-planner/www/mealData.js
@@ -0,0 +1,18 @@
+
+//arr of all vars created with Meal constructor
+var meals = [];
+
+function Meal(type, name, ingredients) {
+  this.type = type;
+  this.name = name;
+  this.ingredients = ingredients;
+
+}
+
+//test meal constructor
+var spagBol = new Meal("dinner", "Spaghetti Bolognese", ["Mince", "Sauce", "Spaghetti", ]);
+
+//generate dropdown options for meal type
+var mealOptions = () => {
+
+}
